3b6c274df9e841412057d4a5258ba38190924eaa
LuanFabricio <luanlimaleite@hotmail.com>
2024-10-02 21:30:05 -0300

    feat: Adding a game loop on minesweeper

--- main.c
+++ main.c
@@ -5,24 +5,18 @@
 
 int main(void)
 {
-	//u32 grid[] =  { 0x00 };//, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
-	//u32 mask[] =  { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
-
 	const u32 WIDTH = 8;
 	const u32 HEIGHT = 8;
 	u32 *grid = minesweep_create_random_grid(WIDTH, HEIGHT);
 	minesweep_t minesweep = minesweep_new(WIDTH, HEIGHT, grid);
 
-	minesweep_flip_blank_neightbors(&minesweep, 1, 1, 0, 5);
+	u32 x, y;
+	while(!minesweep_is_game_done(minesweep)) {
+		minesweep_print(minesweep);
+		scanf("%u %u", &x, &y);
+		minesweep_flip_position(&minesweep, x, y);
+	}
 	minesweep_print(minesweep);
 
-	minesweep_flip_position(&minesweep, 1, 1);
-	minesweep_print(minesweep);
-
-	minesweep.mask[0] = -1;
-	minesweep.mask[1] = -1;
-	minesweep_print(minesweep);
-
-	printf("Hello, world!\n");
 	return 0;
 }
