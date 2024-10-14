74fdbc0c3764381d24df9dffad456eb71f9ff5a9
LuanFabricio <luanlimaleite@hotmail.com>
2024-10-13 22:09:49 -0300

    feat: Adding a header for colors definitions

---
+++ src/core/colors.h
@@ -0,0 +1,10 @@
+#ifndef __COLORS_H__
+#define __COLORS_H__
+
+#define BLACK 	0xff000000
+#define GRAY 	0xff828282
+#define WHITE 	0xffffffff
+#define GREEN	0xff00e430
+#define YELLOW	0xfffdf900
+
+#endif // __COLORS_H__
