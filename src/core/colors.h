6432783d134541157bc824773f160a4e9a0ee85e
LuanFabricio <luanlimaleite@hotmail.com>
2024-10-13 22:47:18 -0300

    fix: Fixing color ordering on colors.h

--- src/core/colors.h
+++ src/core/colors.h
@@ -4,7 +4,7 @@
 #define BLACK 	0xff000000
 #define GRAY 	0xff828282
 #define WHITE 	0xffffffff
-#define GREEN	0xff00e430
-#define YELLOW	0xfffdf900
+#define GREEN	0xff30e400
+#define YELLOW	0xff00f9fd
 
 #endif // __COLORS_H__
